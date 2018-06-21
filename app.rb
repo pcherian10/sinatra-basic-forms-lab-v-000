d5a7f1e718359463af6e1846a151e3c308bc060b
Paul <pcherian10@gmail.com>
2018-06-21 16:10:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do
     @name = params['name']
     @breed = params['breed']
-    @age = paramsp['age']
+    @age = params['age']
     erb :display_puppy
   end
 
